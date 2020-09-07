d21e37185e53a559fdaa5c315867e5e2e63c4e26
yunaichun <yunaichun@qingting.fm>
2020-09-07 16:11:19 +0800

    feat: 接口

---
+++ src/7.接口.ts
@@ -0,0 +1,56 @@
+// 接口是一种类型，不能作为值使用
+interface Point {
+    x: number,
+    y: number,
+}
+let p1: Point = {
+    x: 1,
+    y: 2
+}
+
+
+// == 1.可选属性
+interface Point2 {
+    x: number,
+    y: number,
+    color?: string
+}
+let p2: Point2 = {
+    x: 1,
+    y: 2
+}
+
+
+// == 2.只读属性
+interface Point3 {
+    readonly x: number,
+    readonly y: number,
+    color?: string
+}
+let p3: Point3 = {
+    x: 1,
+    y: 2
+}
+// p3.x = 4;// 错误
+
+
+// == 3.任意属性
+interface Point4 {
+    x: number,
+    y: number,
+    [prop: string]: number
+};
+let p4: Point4 = {
+    x: 1,
+    y: 2,
+    0: 999
+};
+p4.t = 10;
+p4[1] = 1;
+
+
+// == 注意：number 类型必须是 string 类型的子类型
+interface Point5 {
+    [prop1: string]: Object,
+    [prop2: number]: number
+};
