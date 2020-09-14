bcb7216ceb06a4bcb50c9cd3d7dfb18e553460a1
yunaichun <yunaichun@qingting.fm>
2020-09-14 10:52:54 +0800

    feat: 类型保护补充

--- src/8.联合类型|.ts
+++ src/8.联合类型|.ts
@@ -1,4 +1,4 @@
-// == 1、联合类型
+// == 1、联合类型 - 类型保护 - typeof
 function reverse1(x: number | string, y?: string): number | string | null {
     if (typeof x === 'number') {
         return Number(x.toString().split('').reverse().join(''));
@@ -6,4 +6,33 @@ function reverse1(x: number | string, y?: string): number | string | null {
         return x.split('').reverse().join('');
     }
     return null;
+}
+
+
+// == 2、联合类型 - 类型保护 - in
+interface Bird {
+    fly: boolean,
+    sing: () => void
+}
+interface Dog {
+    fly: boolean,
+    bark: () => {}
+}
+function tranAnial(animal: Bird | Dog) {
+    if ('sing' in animal) {
+        animal.sing();
+    } else {
+        animal.bark()
+    }
+}
+
+// == 3、联合类型 - 类型保护 - instanceof
+class NumberObj {
+    count: number = 0;
+}
+function add(first: object|NumberObj, second: object|NumberObj) {
+    if (first instanceof NumberObj && second instanceof NumberObj) {
+        return first.count + second.count;
+    }
+    return 0;
 }
