da722c409c902ccdeae88e112683ca7e704050ba
yunaichun <yunaichun@qingting.fm>
2020-09-14 18:06:28 +0800

    feat: 参数装饰器

---
+++ src/27.参数装饰器.ts
@@ -0,0 +1,16 @@
+// == 参数装饰器
+namespace ParamsDecorator {
+    // 原型，方法名，参数所在的位置
+    function paramDecorator(target: any, method: string, paramIndex: number) {
+        console.log(target, method, paramIndex);
+    }
+
+    class Test {
+        getInfo(name: string, @paramDecorator age: number) {
+            console.log(name, age);
+        }
+    }
+
+    const test = new Test();
+    test.getInfo('Dell', 30);
+}
